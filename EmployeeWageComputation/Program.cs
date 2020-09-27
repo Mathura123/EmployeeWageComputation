431d0ddc538d2eda72a8d6f7ab2dcf002e6558c4
Mathura <mathuradas333@gmail.com>
2020-09-27 19:44:46 +0530

    [Mathura] Add. Added wage code by switch case in UC-4

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,31 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            Console.WriteLine("By Switch Case");
+            Random random = new Random();
+            int EMP_RATE_PER_HOUR=20;
+            //variables
+            int empWage = 0;
+            int empHrs = 0;
+            //Computation
+            int empCheck = random.Next(0, 3);
+            switch(empCheck)
+            {
+                case 1:
+                    empHrs = 4;
+                    Console.WriteLine("Is Part Time");
+                    break;
+                case 2:
+                    empHrs = 8;
+                    Console.WriteLine("Is Full Time");
+                    break;
+                case 0:
+                    empHrs = 0;
+                    Console.WriteLine("Is Absent");
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
         }
     }
 }
