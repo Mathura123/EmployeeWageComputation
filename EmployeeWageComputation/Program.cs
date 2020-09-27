815549f5a1e6613aa5ba073b5802342e05f844b5
Mathura <mathuradas333@gmail.com>
2020-09-28 04:33:56 +0530

    [Mathura] Add. Added day reached or not code to UC-6

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,22 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random randomNo = new Random();
+            int dayReachedOrNOT = randomNo.Next(0, 2);
+            switch(dayReachedOrNOT)
+            {
+                case 0:
+                    empHrs = 100;
+                    break;
+                case 1:
+                    empHrs = 8 * 20;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Emp Wage is "+ empWage);
         }
     }
 }
