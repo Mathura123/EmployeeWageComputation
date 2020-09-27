9dda6b955b1da8e9be95ba537b08e1d38e93ad7d
Mathura <mathuradas333@gmail.com>
2020-09-27 19:23:00 +0530

    [Mathura] Add. Added part time computation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,26 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            //Contants
+            int IS_FULL_TIME = 2;
+            int IS_PART_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs =0;
+            int empWage =0;
+            Random random = new Random();
+            //Computations
+            int empCheck = random.Next(0, 3);
+            if(empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;    
+            }
+            if(empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
